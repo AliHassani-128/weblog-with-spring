b4b0a21eb25a79fb52aa19a6c9cbe52d9e1925c9
Ali <alihassani128@yahoo.com>
2023-01-02 22:35:17 +0330

    fix index api

--- src/main/java/com/example/weblog/Controller/IndexController.java
+++ src/main/java/com/example/weblog/Controller/IndexController.java
@@ -1,24 +1,16 @@
 package com.example.weblog.Controller;
 
-import com.example.weblog.Entity.User;
-import com.example.weblog.Repository.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class IndexController {
-    private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/index")
-    public User index(@RequestParam(value = "name", defaultValue = "World") String name) {
-//        return String.format("Hello %s", name);
-        return new User(counter.incrementAndGet(), "ali", "password", "Ali", "Hassani");
+    public String index(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s", name);
     }
-
 
 }
